74fd1e89a9fc47298ff20cdd1e4b2faf6bc0e15b
BPitts8019 <bradleyj.pitts@gmail.com>
2020-11-10 19:20:35 -0700

    feat: Add basic navigation routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Attributes from "./attributes/Attributes";
 
 function App() {
    return (
       <>
          <Attributes />
-         <h1>Do you wanna Hop on a Plane?</h1>
+         <Switch>
+            <Route
+               exact
+               path="/"
+               component={() => {
+                  return <h1>Do you wanna Hop on a Plane?</h1>;
+               }}
+            />
+            <Route
+               exact
+               path="/login"
+               component={() => {
+                  return <h1>Login Screen</h1>;
+               }}
+            />
+            <Route
+               component={() => {
+                  return <h1>404 Page Not Found</h1>;
+               }}
+            />
+         </Switch>
       </>
    );
 }
